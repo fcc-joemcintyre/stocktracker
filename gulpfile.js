72a688912643a7e0bbaa6ac2b9756c0074e6abbe
Joe McIntyre <j_h_mcintyre@yahoo.com>
2016-06-18 15:36:48 -0500

    Add staging to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,124 +1,135 @@
-"use strict";
-let gulp = require("gulp");
-let gutil = require("gulp-util");
-let gulpif = require("gulp-if");
-let cssmin = require("gulp-cssmin");
-let sass = require("gulp-sass");
-let concat = require("gulp-concat");
-let plumber = require("gulp-plumber");
-let buffer = require("vinyl-buffer");
-let source = require("vinyl-source-stream");
-let babelify = require("babelify");
-let browserify = require("browserify");
-let resolutions = require("browserify-resolutions");
-let watchify = require("watchify");
-let uglify = require("gulp-uglify");
-let sourcemaps = require("gulp-sourcemaps");
-
-let production = process.env.NODE_ENV === "production";
+'use strict';
+let gulp = require ('gulp');
+let gutil = require ('gulp-util');
+let cssmin = require ('gulp-cssmin');
+let sass = require ('gulp-sass');
+let buffer = require ('vinyl-buffer');
+let source = require ('vinyl-source-stream');
+let babelify = require ('babelify');
+let browserify = require ('browserify');
+let watchify = require ('watchify');
+let uglify = require ('gulp-uglify');
+let sourcemaps = require ('gulp-sourcemaps');
 
 let dependencies = [
-  "react",
-  "react-dom",
-  "d3",
-  "socket.io",
-  "react-faux-dom"
+  'react',
+  'react-dom',
+  'd3',
+  'socket.io',
+  'react-faux-dom'
 ];
+let stageDir = '../stocktracker-stage';
+let base = 'dist';
 
-// Copy index.html and favicon.ico to dist
-gulp.task("copy-base", function() {
-    return gulp.src(["src/client/index.html", "src/client/favicon.ico"])
-    .pipe(gulp.dest("dist/public"));
+// setup default, local test and staging tasks
+gulp.task ('default', ['html', 'server', 'styles', 'vendor', 'browserify-watch',
+  'watch']);
+gulp.task ('stage', ['set-stage', 'html', 'server', 'styles', 'vendor-stage',
+  'browserify-stage']);
+
+// set the destination for staging output and copy stage root files
+gulp.task ('set-stage', function () {
+  base = stageDir + '/dist';
+  return gulp.src (['stage/*', 'stage/.*'])
+    .pipe (gulp.dest (stageDir));
 });
 
-gulp.task("watch-base", function() {
-  gulp.watch("src/client/index.html", ["copy-base"]);
+// set watch tasks for continous build
+gulp.task ('watch', function () {
+  gulp.watch ('src/client/index.html', ['html']);
+  gulp.watch ('src/server/*.js', ['server']);
+  gulp.watch (dependencies, ['vendor']);
+  gulp.watch ('src/client/css/*.sass', ['styles']);
 });
 
-// Bundle server and place in dist directory
-gulp.task("server-dist", function() {
-    return gulp.src ("src/server/*.js")
-    .pipe (gulp.dest("dist"));
+// copy index.html and favicon.ico
+gulp.task ('html', function () {
+  return gulp.src (['src/client/index.html', 'src/client/favicon.ico'])
+    .pipe (gulp.dest (base + '/public'));
 });
 
-gulp.task("watch-server", function() {
-  gulp.watch("src/server/*.js", ["server-dist"]);
+// copy server content
+gulp.task ('server', function () {
+  return gulp.src ('src/server/*.js')
+    .pipe (gulp.dest (base));
 });
 
-// Combine all JS libraries into a single file for fewer HTTP requests.
-gulp.task("vendor", function() {
-  return gulp.src([
-  ]).pipe(concat("vendor.js"))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest("dist/public/js"));
+// compile third-party dependencies
+gulp.task ('vendor', function () {
+  return browserify ()
+    .require (dependencies)
+    .bundle ()
+    .pipe (source ('vendor.bundle.js'))
+    .pipe (buffer ())
+    .pipe (uglify ({ mangle: false }))
+    .pipe (gulp.dest (base + '/public/js'));
 });
 
-// Compile third-party dependencies separately for faster performance.
-gulp.task("browserify-vendor", function() {
-  return browserify()
-    .require(dependencies)
-    .plugin(resolutions, "*")
-    .bundle()
-    .pipe(source("vendor.bundle.js"))
-    .pipe(buffer())
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest("dist/public/js"));
+// compile stylesheets
+gulp.task ('styles', function () {
+  return gulp.src ('src/client/css/main.sass')
+    .pipe (sass ().on ('error', sass.logError))
+    .pipe (cssmin ())
+    .pipe (gulp.dest (base + '/public/css'));
 });
 
-// Compile only project files, excluding all third-party dependencies.
-gulp.task("browserify", ["browserify-vendor"], function() {
-  return browserify({ entries: "src/client/components/App.jsx", debug: true })
-    .external(dependencies)
-    .plugin(resolutions, "*")
-    .transform(babelify, { presets: ["es2015", "react"] })
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist/public/js"));
-});
+// compile and package application
+gulp.task ('browserify-watch', function () {
+  let bundler = watchify (browserify ({ entries: 'src/client/components/App.jsx', debug: true }, watchify.args));
+  bundler.external (dependencies);
+  bundler.transform (babelify, { presets: ['es2015', 'react'] });
+  bundler.on ('update', rebundle);
+  return rebundle ();
 
-// Same as browserify task, but will also watch for changes and re-compile.
-gulp.task("browserify-watch", ["browserify-vendor"], function() {
-  let bundler = watchify(browserify({ entries: "src/client/components/App.jsx", debug: true }, watchify.args));
-  bundler.external(dependencies);
-  bundler.transform(babelify, { presets: ["es2015", "react"] });
-  bundler.on("update", rebundle);
-  return rebundle();
-
-  function rebundle() {
-    let start = Date.now();
-    return bundler.bundle()
-      .on("error", function(err) {
-        gutil.log(gutil.colors.red(err.toString()));
+  function rebundle () {
+    let start = Date.now ();
+    return bundler.bundle ()
+      .on ('error', function (err) {
+        gutil.log (gutil.colors.red (err.toString ()));
       })
-      .on("end", function() {
-        gutil.log(gutil.colors.green("Finished rebundling in", (Date.now() - start) + "ms."));
+      .on ('end', function () {
+        gutil.log (gutil.colors.green ('Finished rebundling in', (Date.now () - start) + 'ms.'));
       })
-      .pipe(source("bundle.js"))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write("."))
-      .pipe(gulp.dest("dist/public/js/"));
+      .pipe (source ('bundle.js'))
+      .pipe (buffer ())
+      .pipe (sourcemaps.init ({ loadMaps: true }))
+      .pipe (sourcemaps.write ('.'))
+      .pipe (gulp.dest (base + '/public/js/'));
   }
 });
 
-// Compile Sass stylesheets.
-gulp.task("styles", function() {
-  return gulp.src("src/client/css/main.sass")
-    .pipe(plumber())
-    .pipe(sass())
-    //.pipe(autoprefixer())
-    .pipe(gulpif(production, cssmin()))
-    .pipe(gulp.dest("dist/public/css"));
+// Tasks to prepare staging version of application
+gulp.task ('vendor-stage', function () {
+  process.env.NODE_ENV = 'production';
+  return browserify ()
+    .require (dependencies)
+    .bundle ()
+    .pipe (source ('vendor.bundle.js'))
+    .pipe (buffer ())
+    .pipe (uglify ({ mangle: false }))
+    .pipe (gulp.dest (base + '/public/js'));
 });
 
-gulp.task("watch-sass", function() {
-  gulp.watch("src/client/css/main.sass", ["styles"]);
-});
+gulp.task ('browserify-stage', function () {
+  process.env.NODE_ENV = 'production';
+  let bundler = browserify ({ entries: 'src/client/components/App.jsx', debug: true });
+  bundler.external (dependencies);
+  bundler.transform (babelify, { presets: ['es2015', 'react'] });
+  bundler.on ('update', rebundle);
+  return rebundle ();
 
-gulp.task("default", ["copy-base", "server-dist", "styles", "vendor",
-  "watch-base", "watch-server", "browserify-watch", "watch-sass"]);
-gulp.task("build", ["copy-base", "server-dist", "styles", "vendor", "browserify"]);
+  function rebundle () {
+    let start = Date.now ();
+    return bundler.bundle ()
+      .on ('error', function (err) {
+        gutil.log (gutil.colors.red (err.toString ()));
+      })
+      .on ('end', function () {
+        gutil.log (gutil.colors.green ('Finished rebundling in', (Date.now () - start) + 'ms.'));
+      })
+      .pipe (source ('bundle.js'))
+      .pipe (buffer ())
+      .pipe (uglify ({ mangle: false }))
+      .pipe (gulp.dest (base + '/public/js/'));
+  }
+});
